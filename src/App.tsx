a4af6dcf5474e2281d9610cac0d55bb0b3921355
Alexandr Leshko <sasha.leshko99@gmail.com>
2026-03-03 20:05:17 +0300

    refactor: add path aliases and vite-tsconfig-paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { Layout } from './components/Layout';
-import { CartListPage } from './pages/CartList';
-import { CartDetailPage } from './pages/CartDetail';
-import { NotFoundPage } from './pages/NotFound';
+import { Layout } from '@components/Layout';
+import { CartListPage } from '@pages/CartList';
+import { CartDetailPage } from '@pages/CartDetail';
+import { NotFoundPage } from '@pages/NotFound';
 
 const queryClient = new QueryClient();
 
