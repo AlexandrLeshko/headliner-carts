c7bca18ecb0b5d9ac67a7164788453cf33a4e861
Alexandr Leshko <sasha.leshko99@gmail.com>
2026-03-05 01:07:30 +0300

    feat: ThemeProvider, QueryClient staleTime, global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
+import { ThemeProvider } from '@emotion/react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Layout } from '@components/Layout';
 import { CartListPage } from '@pages/CartList';
 import { CartDetailPage } from '@pages/CartDetail';
 import { NotFoundPage } from '@pages/NotFound';
+import { theme } from './theme';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 5 * 60 * 1000,
+    },
+  },
+});
 
 export const App = () => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<CartListPage />} />
-            <Route path="carts/:id" element={<CartDetailPage />} />
-          </Route>
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      </BrowserRouter>
-    </QueryClientProvider>
+    <ThemeProvider theme={theme}>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<CartListPage />} />
+              <Route path="carts/:id" element={<CartDetailPage />} />
+            </Route>
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </BrowserRouter>
+      </QueryClientProvider>
+    </ThemeProvider>
   );
 }
